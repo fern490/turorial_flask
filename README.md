27b42774857b6c3017e67a0830560ab4f58c63d8
Fernando Valle <jjonathanvalle@gmail.com>
2025-05-14 12:19:44 -0300

    Quedó lo de la base de datos por la mitad

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
 # turorial_flask
-
-# Crear entorno vitual:
-
-# python -m venv .venv
-
-# source .venv/bin/activate
-
-# pip install flask
-
-# flask --version
--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,7 +1,31 @@
 from flask import Flask, url_for, render_template, request
 
+import sqlite3
+
 app = Flask(__name__)
+
+with app.app_context():
+   from . import db
+   db.init_app(app)
+
+def dict_factory(cursor, row):
+   """Arma un diccionario con los valores de la fila."""
+   fields = [column[0] for column in cursor.description]
+   return {key: value for key, value in zip(fields, row)}
+
+def abrirConexion():
+    conexion = sqlite3.connect("datos.sqlite")
+    conexion.row_factory = sqlite3.Row
+    return conexion
+
+def cerrarConexion():
+    global db
+    if db is not None:
+        db.close()
+        db = None
 
 @app.route("/")
 def hola_mundo():
     return "<p>Hello, World!</p>"
+
+
