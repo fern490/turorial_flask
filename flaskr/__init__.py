b8607f0e55cd17eb6faac7f89e613262118392df
Fernando Valle <jjonathanvalle@gmail.com>
2025-05-21 09:35:22 -0300

    definir y acceder a la base de datos

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,7 +5,7 @@ import sqlite3
 app = Flask(__name__)
 
 def abrirConexion():
-    conexion = sqlite3.connect("sakila_master.db.sqlite")
+    conexion = sqlite3.connect("datos.sqlite")
     conexion.row_factory = sqlite3.Row
     return conexion
 
@@ -15,6 +15,12 @@ def cerrarConexion():
         db.close()
         db = None
 
+def dict_factory(cursor, row):
+   """Arma un diccionario con los valores de la fila."""
+   fields = [column[0] for column in cursor.description]
+   return {key: value for key, value in zip(fields, row)}
+
 @app.route("/")
 def hola_mundo():
     return "<p>Hello, World!</p>"
+
