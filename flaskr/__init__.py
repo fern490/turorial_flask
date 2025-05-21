b3ad9cb0da7085d2ad361dd58bc517aa9cefbcd9
Fernando Valle <jjonathanvalle@gmail.com>
2025-05-21 09:30:55 -0300

    setup de la app

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,20 +1,11 @@
-from flask import Flask, url_for, render_template, request
+from flask import Flask, url_for, render_template, request, redirect
 
 import sqlite3
 
 app = Flask(__name__)
 
-with app.app_context():
-   from . import db
-   db.init_app(app)
-
-def dict_factory(cursor, row):
-   """Arma un diccionario con los valores de la fila."""
-   fields = [column[0] for column in cursor.description]
-   return {key: value for key, value in zip(fields, row)}
-
 def abrirConexion():
-    conexion = sqlite3.connect("datos.sqlite")
+    conexion = sqlite3.connect("sakila_master.db.sqlite")
     conexion.row_factory = sqlite3.Row
     return conexion
 
@@ -27,5 +18,3 @@ def cerrarConexion():
 @app.route("/")
 def hola_mundo():
     return "<p>Hello, World!</p>"
-
-
